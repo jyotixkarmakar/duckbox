a67f5bce9e3f95463fd4312f1f4f7930c06ab3e1
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-06-26 14:14:26 +0530

    basic class component:Header

---
+++ src/core/layout/Header.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Header extends Component {
+ 
+ render() {
+ return (
+ <div style={{background:'#000',color:'#5cc'}}>
+     <div> Website Name: Duckbox</div>
+     <div>User name: John Doe</div>
+ </div>
+ );
+ }
+}
+export default Header;
--- src/core/layout/MainContainer.tsx
+++ src/core/layout/MainContainer.tsx
@@ -1,7 +1,11 @@
+import Header from "./Header";
+
 const MainContainer = (props:any) => { 
     const {parentAppName}= props;
     return (
-        <div>Hello {props.parentUserName}, welcome to {parentAppName}</div>
+        <div>Hello {props.parentUserName}, welcome to {parentAppName}
+        <Header></Header>
+        </div>
     )
 }
 export default MainContainer;
