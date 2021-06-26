a08ddebf19de2e4f4d5274f16a5ec7cafb4625bc
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-06-26 17:28:43 +0530

    event handler

--- src/core/layout/Header.tsx
+++ src/core/layout/Header.tsx
@@ -5,12 +5,22 @@ class Header extends Component<any, any> {
         this.state = {
             usersAddress: "Kolkata"
         }
+        this.changeCompanyAddress=this.changeCompanyAddress.bind(this);
     }
+    changeCompanyAddress() {
+        this.setState(() => ({
+            usersAddress: "London"
+        }));
+      }
+      
     render() {
         return (
             <div style={{ background: '#000', color: '#5cc' }}>
                 <div> Website Name: Duckbox, Company Address: {this.props.companyAddress}</div>
                 <div>User name: John Doe, User Address: {this.state.usersAddress}</div>
+                <button onClick={this.changeCompanyAddress}>
+                    Change Company Address
+                </button>
             </div>
         );
     }
