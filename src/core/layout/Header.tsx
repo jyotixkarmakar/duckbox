c206874a849d8b9b18ff1afd4db108691668de3e
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-06-26 17:00:07 +0530

    Constructor,state,props

--- src/core/layout/Header.tsx
+++ src/core/layout/Header.tsx
@@ -1,14 +1,18 @@
 import { Component } from "react";
-
-class Header extends Component {
- 
- render() {
- return (
- <div style={{background:'#000',color:'#5cc'}}>
-     <div> Website Name: Duckbox</div>
-     <div>User name: John Doe</div>
- </div>
- );
- }
+class Header extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            usersAddress: "Kolkata"
+        }
+    }
+    render() {
+        return (
+            <div style={{ background: '#000', color: '#5cc' }}>
+                <div> Website Name: Duckbox, Company Address: {this.props.companyAddress}</div>
+                <div>User name: John Doe, User Address: {this.state.usersAddress}</div>
+            </div>
+        );
+    }
 }
 export default Header;
--- src/core/layout/MainContainer.tsx
+++ src/core/layout/MainContainer.tsx
@@ -2,9 +2,10 @@ import Header from "./Header";
 
 const MainContainer = (props:any) => { 
     const {parentAppName}= props;
+    let headOffice="New York"
     return (
         <div>Hello {props.parentUserName}, welcome to {parentAppName}
-        <Header></Header>
+        <Header companyAddress={headOffice}></Header>
         </div>
     )
 }
