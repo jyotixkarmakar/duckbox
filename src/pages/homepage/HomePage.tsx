095eae12932e3193aafb1aad2923d2608e1c6288
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-06-27 00:00:42 +0530

    useState example

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const HomePage = (props: any) => {
+    let storename = "Monginish"
+    const changeStore = () => {
+        storename = "A B Sweets";
+    }
+    const changeShop = () => {
+        setShopName("Big Bazar")
+    }
+    const [shopName, setShopName] = useState("BBQ");
     return (
         <div>
             <h1>Essentials delivered to your doorstep</h1>
-            <div style={{ display: "flex",textAlign:"center" }}>
+            <div style={{ display: "flex", textAlign: "center" }}>
                 <div style={{ width: "32%", margin: "1%", height: "100px", background: "#eee" }}>
                     <h1>Grocery</h1>
                 </div>
@@ -13,6 +23,14 @@ const HomePage = (props: any) => {
                     <h1>Fruit</h1>
                 </div>
             </div>
+            <div>Store name : {storename}</div>
+            <button onClick={changeStore}>
+                change store
+            </button>
+            <div>Shop name : {shopName}</div>
+            <button onClick={changeShop}>
+                change shop
+            </button>
         </div>
     )
 }
