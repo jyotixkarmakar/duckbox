497a76bd35583e303a83c21a040d6df2909bb3f8
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-06-26 22:18:57 +0530

    updating homepage html

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,6 +1,19 @@
-const HomePage = (props:any) => { 
+const HomePage = (props: any) => {
     return (
-        <div>HomePage</div>
+        <div>
+            <h1>Essentials delivered to your doorstep</h1>
+            <div style={{ display: "flex",textAlign:"center" }}>
+                <div style={{ width: "32%", margin: "1%", height: "100px", background: "#eee" }}>
+                    <h1>Grocery</h1>
+                </div>
+                <div style={{ width: "32%", margin: "1%", height: "100px", background: "#eee" }}>
+                    <h1>Vegetable</h1>
+                </div>
+                <div style={{ width: "32%", margin: "1%", height: "100px", background: "#eee" }}>
+                    <h1>Fruit</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 export default HomePage;
