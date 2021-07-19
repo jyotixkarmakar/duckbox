e736bf3033c23a85816749452709a206b220a0a6
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-07-20 00:36:59 +0530

    header basic styles

--- src/core/layout/Header.tsx
+++ src/core/layout/Header.tsx
@@ -1,21 +1,69 @@
+import { Container, Grid, Select, Typography } from "@material-ui/core";
 import { Component } from "react";
 import { Link } from "react-router-dom";
-class Header extends Component<any, any> {
-
-    constructor(props: any) {
-        super(props);
-           }
-
-
-    render() {
-        return (
-            <div style={{ background: '#000', color: '#5cc' }}>
-               sadasdsd
-            </div>
-        );
+import { useStyles } from "./HeadersStyles";
+import logo from "./../../assets/logo.png"
+import { useState } from "react";
+const Header = (props: any) => {
+    const classes = useStyles();
+    const [city, setCity] = useState("Raiganj");
+    const changeCity = (event) => {
+        setCity(event.target.value)
     }
-
-
-
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                <Grid item xs={2}>
+                    <Typography>
+                        <img src={logo}></img>
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography>
+                        <Select
+                            native
+                            value={city}
+                            onChange={changeCity}
+                            inputProps={{
+                                name: 'city',
+                                id: 'city-id',
+                            }}
+                        >
+                            <option value="Raiganj">Raiganj</option>
+                            <option value="Kolkata">Kolkata</option>
+                            <option value="Balurghat">Balurghat</option>
+                        </Select>
+                    </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <Typography>
+                                Duckbox for Partners</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography>Business With Duckbox</Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={3}>
+                    <Grid container>
+                        <Grid item xs={4}>
+                            Search
+                        </Grid>
+                        <Grid item xs={4}>
+                            Cart
+                        </Grid>
+                        <Grid item xs={4}>
+                            Sign In
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} className={classes.headerBottom}>
+                    fgfhgfhg
+                </Grid>
+            </Grid>
+        </div>);
 }
+
 export default Header;
---
+++ src/core/layout/HeadersStyles.tsx
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/styles";
+
+export const useStyles = makeStyles({
+    root: {
+        height: "90px",
+        padding: "0 12%",
+        display: "flex",
+        alignItems: "center"
+    },
+    headerBottom:{
+        color:"#0ca",
+        backgroundColor:"#000",
+        height:"30px",
+        widgh:"100%"
+    }
+});
