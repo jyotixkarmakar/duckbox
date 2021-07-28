dd08363ecabd8f70a4858f1c66eb698f238c9cca
jyoti-bsl <68667125+jyoti-bsl@users.noreply.github.com>
2021-07-29 00:08:08 +0530

    promise and rest api

--- src/pages/CatagoryDetails/CategoryDetails.tsx
+++ src/pages/CatagoryDetails/CategoryDetails.tsx
@@ -10,26 +10,62 @@ const CategoryDetails = (props: any) => {
   let { catagoryName } = useParams<any>();
   const m1 = async () => {
     let shops: any;
+
+    //promise,await,try catch
+    const x = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let i = Math.floor(Math.random() * 101);
+        console.log(i)
+        if (i % 2 == 0) {
+          resolve(111);
+        }
+        else {
+          reject(222)
+        }
+      }, 10000)
+    });
+    try {
+      shops = await x;
+    } catch (ex) {
+      shops = 55555;
+    }
+    let a: any = [4, 6];
+    try {
+      console.log(a.getheight())
+    } catch (ex) {
+      console.log("abc")
+    }
+    console.log("Shoplist", shops);
+
+    //REST API example
     shops = await axios.get('https://60fffde9bca46600171cf670.mockapi.io/shop');
     console.log(shops);
-    let id=Math.floor(Math.random() * 101);
+    let id = Math.floor(Math.random() * 101);
     let newShop = {
       createdAt: "2021-07-27T09:32:46.083Z",
-      name: "My Vodafone shop no:"+id,
+      name: "My Vodafone shop no:" + id,
       avatar: "https://cdn.fakercloud.com/avatars/brandclay_168.jpg"
     }
-    let resp = await axios.post('https://60fffde9bca46600171cf670.mockapi.io/shop', newShop)
-    
+    // let resp = await axios.post('https://60fffde9bca46600171cf670.mockapi.io/shop', newShop)
+
     let newShop1 = {
       createdAt: "2021-07-27T09:32:46.083Z",
-      name: "My updated Vodafone shop no:"+id,
+      name: "My updated Vodafone shop no:" + id,
       avatar: "https://cdn.fakercloud.com/avatars/brandclay_168.jpg"
     }
+    // try{
     let resp1 = await axios.put('https://60fffde9bca46600171cf670.mockapi.io/shop/2', newShop)
-    
+    // }
+    // catch(e){
+    //   console.log(e)
+    // }
     let resp2 = await axios.delete('https://60fffde9bca46600171cf670.mockapi.io/shop/2')
-    
-    console.log(resp);
+
+    console.log(resp2);
+
+
+
+
   }
 
   useEffect(() => {
